7ba91f03892c144c23cf129d50cf984b5c7d869e
matscus <matscus@yandex.ru>
2019-11-08 13:11:51 +0000

    add handbook handlers

---
+++ MicroServices/admins/handlers/Handbook_handlers.go
@@ -0,0 +1,75 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/matscus/Hamster/Package/Clients/client"
+	"github.com/matscus/Hamster/Package/Projects/projects"
+)
+
+//GetAllProjects -  return all projects
+func GetAllProjects(w http.ResponseWriter, r *http.Request) {
+	allproject, err := client.PGClient{}.New().GetAllProjects()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("{\"Message\":\"" + err.Error() + "\"}"))
+	} else {
+		err := json.NewEncoder(w).Encode(allproject)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("{\"Message\":\"" + err.Error() + "\"}"))
+		}
+	}
+}
+
+//Projects -  create new Project, update Project and delete Project
+func Projects(w http.ResponseWriter, r *http.Request) {
+	project := projects.Project{}
+	err := json.NewDecoder(r.Body).Decode(&project)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("{\"Message\":\"" + err.Error() + "\"}"))
+	} else {
+		switch r.Method {
+		case "POST":
+			ok, err := project.IfExist()
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("{\"Message\":\"" + err.Error() + "\"}"))
+			} else {
+				if ok {
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte("{\"Message\": Host is exist }"))
+				} else {
+					err = project.Create()
+					if err != nil {
+						w.WriteHeader(http.StatusInternalServerError)
+						w.Write([]byte("{\"Message\":\"" + err.Error() + "\"}"))
+					} else {
+						w.WriteHeader(http.StatusOK)
+						w.Write([]byte("{\"Message\":\"User created \"}"))
+					}
+				}
+			}
+		case "PUT":
+			err = project.Update()
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("{\"Message\":\"" + err.Error() + "\"}"))
+			} else {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte("{\"Message\":\"Host updated \"}"))
+			}
+		case "DELETE":
+			err = project.Delete()
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("{\"Message\":\"" + err.Error() + "\"}"))
+			} else {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte("{\"Message\":\"Host deleted \"}"))
+			}
+		}
+	}
+}
