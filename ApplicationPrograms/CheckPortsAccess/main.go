bb2d198a1795c71fbe11e5ff333c437529b2a679
matscus <matscus@yandex.ru>
2019-11-21 20:24:45 +0000

    rename var

--- ApplicationPrograms/CheckPortsAccess/main.go
+++ ApplicationPrograms/CheckPortsAccess/main.go
@@ -17,7 +17,7 @@ type config struct {
 		Ports []string `yaml:"ports"`
 	} `yaml:"Multi_hosts"`
 	SingleHost []struct {
-		Hosts string   `yaml:"hosts"`
+		Host  string   `yaml:"host"`
 		Ports []string `yaml:"ports"`
 	} `yaml:"Single_host"`
 }
@@ -54,16 +54,16 @@ func main() {
 	}
 	for _, host := range config.SingleHost {
 		for _, port := range host.Ports {
-			_, err = d.Dial("tcp", host.Hosts+":"+port)
+			_, err = d.Dial("tcp", host.Host+":"+port)
 			if err != nil {
 				ok := os.IsTimeout(err)
 				if ok {
-					res = append(res, result{host: host.Hosts, port: port, status: "Fail"})
+					res = append(res, result{host: host.Host, port: port, status: "Fail"})
 				} else {
-					res = append(res, result{host: host.Hosts, port: port, status: "Pass"})
+					res = append(res, result{host: host.Host, port: port, status: "Pass"})
 				}
 			} else {
-				res = append(res, result{host: host.Hosts, port: port, status: "Pass"})
+				res = append(res, result{host: host.Host, port: port, status: "Pass"})
 			}
 		}
 	}
