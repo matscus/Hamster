cd1dc59d29c30b98e714cf982a4b8c40c1beee5c
matscus <matscus@yandex.ru>
2020-01-14 11:03:45 +0000

    add owner

--- Package/Services/service/ServiceImpl.go
+++ Package/Services/service/ServiceImpl.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"io"
+	"os"
 	"sync"
 
 	"github.com/matscus/Hamster/Package/Clients/client"
@@ -10,14 +12,15 @@ import (
 //launch status, web address and mutex for blocking parallel changes in the launch status by different threads
 type Service struct {
 	Mutex    *sync.Mutex `json:",omitempty"`
-	ID       int64       `json:"id"`
-	Name     string      `json:"name"`
-	Host     string      `json:"host"`
-	Status   string      `json:"status"`
-	URI      string      `json:"uri"`
-	Type     string      `json:"type"`
-	Projects []string    `json:"projects"`
+	ID       int64       `json:"id,omitempty"`
+	Name     string      `json:"name,omitempty"`
+	Host     string      `json:"host,omitempty"`
+	Status   string      `json:"status,omitempty"`
+	URI      string      `json:"uri,omitempty"`
+	Type     string      `json:"type,omitempty"`
+	Projects []string    `json:"projects,omitempty"`
 	RunSTR   string      `json:"runstr,omitempty"`
+	Own      string      `json:"owner,omitempty"`
 }
 
 //Run  - service run function. performs connection to the host via ssh.
@@ -43,9 +46,43 @@ func (s *Service) Stop(user string) error {
 	return err
 }
 
-//Create - insert new generator to database
-func (s *Service) Create() error {
-	return client.PGClient{}.New().NewService(s.Name, s.Host, s.URI, s.Type, s.RunSTR, s.Projects)
+//CreateBin - create bin from bins dir and insert data from tBins
+func (s *Service) CreateBin(f io.Reader, own string) error {
+	newFile := os.Getenv("BINSDIR") + "/" + s.Type + "/" + s.Name + ".zip"
+	file, err := os.OpenFile(newFile, os.O_CREATE|os.O_RDWR, os.FileMode(0755))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = io.Copy(file, f)
+	if err != nil {
+		return err
+	}
+	pgclient := client.PGClient{}.New()
+	projectsID, err := pgclient.GetProjectsIDtoString(s.Projects)
+	if err != nil {
+		return err
+	}
+	return client.PGClient{}.New().NewServiceBin(s.Name, s.Type, s.RunSTR, own, projectsID)
+}
+
+//UpdateBin - update bins and data from tBins
+func (s *Service) UpdateBin(own string) error {
+	return client.PGClient{}.New().UpdateServiceBin(s.ID, s.Name, s.Type, s.RunSTR, own)
+}
+
+//DeleteBin - delete bins and data from tBins
+func (s *Service) DeleteBin() error {
+	err := os.Remove(os.Getenv("BINSDIR") + "/" + s.Type + "/" + s.Name + ".zip")
+	if err != nil {
+		return err
+	}
+	return client.PGClient{}.New().DeleteServiceBin(s.ID)
+}
+
+//Create - insert new service to database
+func (s *Service) Create(owner string) error {
+	return client.PGClient{}.New().NewService(s.Name, s.Host, s.URI, s.Type, s.RunSTR, s.Projects, owner)
 }
 
 //Update - update service info from database
@@ -72,7 +109,7 @@ func (s *Service) InstallServiceToRemoteHost(user string) (err error) {
 		return err
 	}
 	pgclient := client.PGClient{}.New()
-	err = pgclient.NewService(s.Name, s.Host, s.URI, s.Type, s.RunSTR, s.Projects)
+	err = pgclient.NewService(s.Name, s.Host, s.URI, s.Type, s.RunSTR, s.Projects, user)
 	if err != nil {
 		return err
 	}
