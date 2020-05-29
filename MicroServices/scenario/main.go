1902881941511c2f1d397754baca2f049877200f
matscus <matscus@yandex.ru>
2020-05-29 16:38:36 +0000

    add addr

--- MicroServices/scenario/main.go
+++ MicroServices/scenario/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -24,6 +25,7 @@ var (
 	keyPath      string
 	proto        string
 	listenport   string
+	host         string
 	wait         time.Duration
 	writeTimeout time.Duration
 	readTimeout  time.Duration
@@ -54,12 +56,6 @@ func main() {
 	flag.DurationVar(&idleTimeout, "idle-timeout", time.Second*60, "idle server timeout")
 	flag.Parse()
 	r := mux.NewRouter()
-	srv := &http.Server{
-		Addr:         "0.0.0.0:" + listenport,
-		WriteTimeout: writeTimeout,
-		ReadTimeout:  readTimeout,
-		IdleTimeout:  idleTimeout,
-	}
 	r.HandleFunc("/api/v1/scenario/start", middleware.Middleware(handlers.StartScenario)).Methods(http.MethodPost, http.MethodOptions)
 	r.HandleFunc("/api/v1/scenario/stop", middleware.Middleware(handlers.StopScenario)).Methods(http.MethodPost, http.MethodOptions)
 	r.HandleFunc("/api/v1/scenario/new", middleware.Middleware(handlers.NewScenario)).Methods(http.MethodPost, http.MethodOptions)
@@ -70,7 +66,29 @@ func main() {
 	r.PathPrefix("/api/v1/scenario/files/").Handler(http.StripPrefix("/api/v1/scenario/files/", middleware.MiddlewareFiles(http.FileServer(http.Dir("/home/matscus/Hamster/projects/"))))).Methods(http.MethodGet, http.MethodOptions)
 	r.HandleFunc("/api/v1/scenario/precheck", middleware.Middleware(handlers.PreCheckScenario)).Methods(http.MethodPost, http.MethodOptions)
 	http.Handle("/api/v1/", r)
-	//r.Use(mux.CORSMethodMiddleware(r))
+
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		log.Println("Get interface adres error: ", err.Error())
+		os.Exit(1)
+	}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				os.Stdout.WriteString(ipnet.IP.String() + "\n")
+				host = ipnet.IP.String()
+			}
+		}
+	}
+	srv := &http.Server{
+		Addr:         host + ":" + listenport,
+		WriteTimeout: writeTimeout,
+		ReadTimeout:  readTimeout,
+		IdleTimeout:  idleTimeout,
+		Handler:      r,
+	}
+
 	go func() {
 		switch proto {
 		case "https":
